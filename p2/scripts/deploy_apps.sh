dd334d275ce8e301a60fa301a017c41be07c12ab
shima8823 <taiyaki1275@gmail.com>
2025-05-05 18:36:24 +0900

    feat(deployment): add script to apply Kubernetes YAML configurations
    This commit introduces a new deployment script that applies all YAML configuration files located in /home/vagrant/confs/ using kubectl. The script iterates over the files and applies each one if present, providing feedback on the application process. In the event that no YAML files are found, the script exits with an error message. This addition streamlines the deployment process by automating configuration management.

---
+++ p2/scripts/deploy_apps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# applay all confs/*.yaml
+for file in /home/vagrant/confs/*/*.yaml; do
+  if [ -f "$file" ]; then
+    echo "Applying $file..."
+    sudo kubectl apply -f "$file"
+  else
+    echo "No YAML files found in /home/vagrant/confs/"
+    exit 1
+  fi
+done
