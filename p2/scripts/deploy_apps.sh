62c2980c7074f0a715375acd18829f19fb6b9fd9
shima8823 <taiyaki1275@gmail.com>
2025-05-12 17:48:09 +0900

    feat(deploy): アプリケーションのデプロイメントスクリプトを更新し、ポッドの準備を待機する機能を追加
    各アプリケーションの設定ファイルを適用する際に、ポッドが準備完了になるまで待機する処理を追加しました。これにより、デプロイメントプロセスが安定し、すべてのポッドが正しく起動することが保証されます。また、ingress-nginxの適用を削除しました。

--- p2/scripts/deploy_apps.sh
+++ p2/scripts/deploy_apps.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 
-# apply ingress-nginx
-sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
-
 # apply all confs/*.yaml
+sudo kubectl apply -f /home/vagrant/confs/app-one/config.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-one/deploy.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-one/service.yaml
+
+sudo kubectl apply -f /home/vagrant/confs/app-two/config.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-two/deploy.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-two/service.yaml
+
+sudo kubectl apply -f /home/vagrant/confs/app-three/config.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-three/deploy.yaml
 sudo kubectl apply -f /home/vagrant/confs/app-three/service.yaml
+
+# # wait for all pods to be ready
+until kubectl get pods -l app=app-one | grep -v NAME; do
+  echo "Waiting for pods to be created..."
+  sleep 2
+done
+
+until kubectl get pods -l app=app-two | grep -v NAME; do
+  echo "Waiting for pods to be created..."
+  sleep 2
+done
+
+until kubectl get pods -l app=app-three | grep -v NAME; do
+  echo "Waiting for pods to be created..."
+  sleep 2
+done
+
+sudo kubectl wait --for=condition=ready pod --all --timeout=300s
 sudo kubectl apply -f /home/vagrant/confs/ingress.yaml
