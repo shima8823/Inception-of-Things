d344a1ef8c034de453c155ad929437918f97b303
shima8823 <taiyaki1275@gmail.com>
2025-05-20 16:40:25 +0900

    feat(scripts): argocdのURLと初期パスワードを表示する機能を追加

--- p3/scripts/setup-argocd.sh
+++ p3/scripts/setup-argocd.sh
@@ -18,3 +18,5 @@ kubectl apply -f ./confs/application.yaml
 # stdout and stderr to /dev/null
 kubectl port-forward svc/argocd-server -n argocd 8080:80 > /dev/null 2>&1 &
 echo "argocd processId: $\!"
+echo "argocd url: http://127.0.0.1:8080"
+echo "argocd password: $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)"
