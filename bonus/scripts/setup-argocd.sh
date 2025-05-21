dcad4e101649b1f57dd60296b43b3dec5ab1aa66
shima8823 <taiyaki1275@gmail.com>
2025-05-21 16:22:51 +0900

    feat(scripts): copy p3, k3dとargocdのセットアップを自動化するスクリプトを追加

---
+++ bonus/scripts/setup-argocd.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+k3d cluster create iot-shshimad --port 8888:8888@loadbalancer
+
+kubectl create namespace dev
+kubectl create namespace argocd
+
+# argocd install
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+# wait for argocd pods to be ready 
+kubectl rollout status -n argocd deployments --timeout=300s
+
+# create argocd app
+kubectl apply -f ./confs/application.yaml
+
+# argocd port forward
+# stdout and stderr to /dev/null
+kubectl port-forward svc/argocd-server -n argocd 8080:80 > /dev/null 2>&1 &
+echo "argocd processId: $\!"
+echo "argocd url: http://127.0.0.1:8080"
+echo "argocd password: $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)"
