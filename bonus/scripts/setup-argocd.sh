8e26875e99627d4cc280e2076f42b53938555dae
shima8823 <taiyaki1275@gmail.com>
2025-06-19 15:31:22 +0900

    feat(scripts): GitLabのセットアップを自動化するスクリプトを追加

--- bonus/scripts/setup-argocd.sh
+++ bonus/scripts/setup-argocd.sh
@@ -2,6 +2,17 @@
 
 k3d cluster create iot-shshimad-bonus --port 8888:8888@loadbalancer
 
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+
+helm upgrade --install gitlab gitlab/gitlab \
+  --timeout 600s \
+  -f confs/values.yaml \
+  --namespace gitlab \
+  --create-namespace \
+  --set certmanager-issuer.email=me@example.com \
+  --set global.edition=ce
+
 kubectl create namespace dev
 kubectl create namespace argocd
 
@@ -20,3 +31,11 @@ kubectl port-forward svc/argocd-server -n argocd 8080:80 > /dev/null 2>&1 &
 echo "argocd processId: $\!"
 echo "argocd url: http://127.0.0.1:8080"
 echo "argocd password: $(kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath="{.data.password}" | base64 -d)"
+
+# gitlab port forward
+
+kubectl port-forward svc/gitlab-webservice-default -n gitlab 8081:8181 --address 0.0.0.0 > /dev/null 2>&1 &
+
+echo "gitlab processId: $\!"
+echo "gitlab url: http://127.0.0.1:8081"
+echo "gitlab password: $(kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath="{.data.password}" | base64 -d)"
