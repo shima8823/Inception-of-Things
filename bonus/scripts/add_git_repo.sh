397dd8cead857a3be801808fc498c5c9ce04e313
shima8823 <taiyaki1275@gmail.com>
2025-06-19 19:03:42 +0900

    feat(scripts): GitLabリポジトリを追加するスクリプトを作成

---
+++ bonus/scripts/add_git_repo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# .envファイルを読み込む
+if [ -f .env ]; then
+  source .env
+else
+  echo ".envファイルが見つかりません"
+  exit 1
+fi
+
+if [ -z "$GITLAB_TOKEN" ]; then
+  echo "エラー: GITLAB_TOKENが設定されていません。"
+  exit 1
+fi
+
+curl --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
+     --header "Content-Type: application/json" \
+     -X POST "http://localhost:8081/api/v4/projects" \
+     --data '{"name":"IoT-shshimad","import_url":"https://github.com/shima8823/IoT-shshimad","visibility":"public"}'
