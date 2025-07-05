96c9ff522975d7035ebfad201093a1453b81fc1a
shima8823 <taiyaki1275@gmail.com>
2025-07-05 16:33:55 +0900

    fix(Vagrantfile, setup_k3s.sh): remove inline shell provision and move setup commands to setup_k3s.sh

--- p2/scripts/setup_k3s.sh
+++ p2/scripts/setup_k3s.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y curl wget git vim
+echo "alias k='sudo k3s kubectl'" >> /home/vagrant/.bashrc
+
 # Donwlaod and install k3s
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110" sh -
